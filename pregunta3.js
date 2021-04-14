b869b30489b06f5b256aed7208e331de08cf2557
IsabelSayagoB <40145915+IsabelSayagoB@users.noreply.github.com>
2021-04-13 22:34:16 -0300

    Isabel Sayago 2

--- pregunta3.js
+++ pregunta3.js
@@ -2,7 +2,6 @@
 // Por ejemplo en: [4,3,4,5,1] el tercer mayor es 3
 // si la longitud del array es < 3 se debe retornar undefined
 
-const array = [1,2,4,5]
 
 const greater3 = nums => {
     
@@ -34,4 +33,3 @@ console.log(greater3([4,3,4,5,1]) === 3);
 console.log(greater3([3,4]) === undefined);
 console.log(greater3([4,4,4]) === 4);
 console.log(greater3([1,1,2,5]) === 1);
-console.log(greater3([1,1,2,5]) === 5);
